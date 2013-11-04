3b8115b85b1afc08418cbbdd967aa689e7eb393f
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-04 14:03:04 +0200

    Revert "We need static prefix for Prod deployment"
    This reverts commit 44d2aafc6a3122fbc5ea08e3d6c7b7dc625de7ac.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Static::Application.routes.draw do
 
-  scope "/static" do
-    controller "root", :format => false do
-      get "/templates/:template.raw.html.erb" => :raw_template
-      get "/templates/:template.html.erb" => :template
-    end
-
-    # Icon redirects
-    get "/favicon.ico", :to => "icon_redirects#show"
-    get "/apple-touch-icon.png", :to => "icon_redirects#apple_57_57"
-    get "/apple-touch-icon-144x144.png", :to => "icon_redirects#show"
-    get "/apple-touch-icon-114x114.png", :to => "icon_redirects#show"
-    get "/apple-touch-icon-72x72.png", :to => "icon_redirects#show"
-    get "/apple-touch-icon-57x57.png", :to => "icon_redirects#show"
-    get "/apple-touch-icon-precomposed.png", :to => "icon_redirects#apple_57_57"
+  controller "root", :format => false do
+    get "/templates/:template.raw.html.erb" => :raw_template
+    get "/templates/:template.html.erb" => :template
   end
+
+  # Icon redirects
+  get "/favicon.ico", :to => "icon_redirects#show"
+  get "/apple-touch-icon.png", :to => "icon_redirects#apple_57_57"
+  get "/apple-touch-icon-144x144.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-114x114.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-72x72.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-57x57.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-precomposed.png", :to => "icon_redirects#apple_57_57"
 end
